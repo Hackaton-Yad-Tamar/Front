488f957dde6fbad6b12bc57449be235356e169a9
Nadav Altabet <nadavalt1@proton.me>
2025-03-05 13:24:44 +0200

    Feat: Add i18n with an example

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { useTranslation } from "react-i18next";
+import "./i18n/config";
+import { Namespace } from "./i18n/namespaces";
+
 const App = () => {
-    return (
-        <div></div>
-    );
+  const { t: tProfileView } = useTranslation(Namespace.profileView);
+  return <div>{tProfileView("example")}</div>;
 };
 
 export default App;
