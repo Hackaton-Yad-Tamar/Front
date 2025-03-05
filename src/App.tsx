7591c6ac93bc44888b704b25ca104cefbde88611
Afik <afikhaim@gmail.com>
2025-03-05 14:04:14 +0200

    add theme colors

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./Home";
 
+export const themeColors = {
+    lightBlue: "#00AEEE",
+    lightGreen: "#67B446",
+    darkBlue: "#002F42",
+    orange: "#FF8E00"
+};
+
 const App: React.FC = () => {
     return (
       <Router>
