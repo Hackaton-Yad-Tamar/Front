6157d1a744946c4c0e1c95a4117cbf67fddb382e
edengal283 <eden283gal@gmail.com>
2025-03-06 04:42:49 +0200

    finish

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -4,146 +4,150 @@ import createCache from "@emotion/cache";
 import { CacheProvider } from "@emotion/react";
 import rtlPlugin from "stylis-plugin-rtl";
 import {
-  forgotPasswordStyle,
-  signInButtonStyle,
-  signInTextFieldStyle,
+    forgotPasswordStyle,
+    signInButtonStyle,
+    signInTextFieldStyle,
 } from "./styles";
 import { FirstSignInDialog } from "../FirstSignInDialog/FirstSignInDialog";
 import { saveData } from '../../api/axios';
 import { SHA256 } from "crypto-js";
 import { useUser } from "../../contexts/userContext";
+import { useNavigate } from "react-router-dom";
 
 interface SignInForm {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 }
 
 const rtlCache = createCache({
-  key: "muirtl",
-  stylisPlugins: [rtlPlugin],
+    key: "muirtl",
+    stylisPlugins: [rtlPlugin],
 });
 
 const SignIn: React.FC = () => {
-  const [formData, setFormData] = useState<SignInForm>({
-    email: "",
-    password: "",
-  });
-  const [errors, setErrors] = useState<{ email?: string; password?: string }>(
-    {}
-  );
-  const [isFirstTime, setIsFirstTime] = useState<boolean>(false);
-  const {login} = useUser();
+    const [formData, setFormData] = useState<SignInForm>({
+        email: "",
+        password: "",
+    });
+    const [errors, setErrors] = useState<{ email?: string; password?: string }>(
+        {}
+    );
+    const [isFirstTime, setIsFirstTime] = useState<boolean>(false);
+    const { login } = useUser();
+    const navigate = useNavigate()
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
 
-  const validateForm = (): boolean => {
-    let formErrors: { email?: string; password?: string } = {};
-    let isValid = true;
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
 
-    if (!formData.email) {
-      formErrors.email = "מייל דרוש";
-      isValid = false;
-    }
+    const validateForm = (): boolean => {
+        let formErrors: { email?: string; password?: string } = {};
+        let isValid = true;
 
-    if (!formData.password) {
-      formErrors.password = "סיסמה דרושה";
-      isValid = false;
-    }
+        if (!formData.email) {
+            formErrors.email = "מייל דרוש";
+            isValid = false;
+        }
 
-    setErrors(formErrors);
-    return isValid;
-  };
+        if (!formData.password) {
+            formErrors.password = "סיסמה דרושה";
+            isValid = false;
+        }
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+        setErrors(formErrors);
+        return isValid;
+    };
 
-    if (validateForm()) {
-      const user = await saveData('http://localhost:8000/users/signin', { email: formData.email, password: SHA256(formData.password).toString() });
-      
-      if(!user.first_sign_in){
-        login(user)
-      }
-      setIsFirstTime(user.first_sign_in);
-    
-    } else {
-      console.log("לטופס יש שגיאות");
-    }
-  };
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
 
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#f5f5f5",
-        direction: "rtl",
-      }}
-    >
-      <CacheProvider value={rtlCache}>
+        if (validateForm()) {
+            const user = await saveData('http://localhost:8000/users/signin', { email: formData.email, password: SHA256(formData.password).toString() });
+
+            if (!user.first_sign_in) {
+                login(user)
+                navigate("/home")
+            }
+            setIsFirstTime(user.first_sign_in);
+
+        } else {
+            console.log("לטופס יש שגיאות");
+        }
+    };
+
+    return (
         <Box
-          component="form"
-          onSubmit={handleSubmit}
-          sx={{
-            width: "19vw",
-            padding: "1.5vw",
-            backgroundColor: "white",
-            borderRadius: "8px",
-            boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
-          }}
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#f5f5f5",
+                direction: "rtl",
+            }}
         >
-          <Typography
-            variant="h5"
-            sx={{ marginBottom: "4vh", textAlign: "center" }}
-          >
-            כניסה למשתמש קיים
-          </Typography>
+            <CacheProvider value={rtlCache}>
+                <Box
+                    component="form"
+                    onSubmit={handleSubmit}
+                    sx={{
+                        width: "19vw",
+                        padding: "1.5vw",
+                        backgroundColor: "white",
+                        borderRadius: "8px",
+                        boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
+                    }}
+                >
+                    <Typography
+                        variant="h5"
+                        sx={{ marginBottom: "4vh", textAlign: "center" }}
+                    >
+                        כניסה למשתמש קיים
+                    </Typography>
 
-          <TextField
-            label="מייל"
-            variant="outlined"
-            fullWidth
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            error={!!errors.email}
-            helperText={errors.email}
-            sx={signInTextFieldStyle}
-          />
+                    <TextField
+                        label="מייל"
+                        variant="outlined"
+                        fullWidth
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        error={!!errors.email}
+                        helperText={errors.email}
+                        sx={signInTextFieldStyle}
+                    />
 
-          <TextField
-            label="סיסמה"
-            variant="outlined"
-            type="password"
-            fullWidth
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            error={!!errors.password}
-            helperText={errors.password}
-            sx={signInTextFieldStyle}
-          />
-          <Box sx={{ display: "flex", justifyContent: "center" }}>
-            <Button variant="contained" type="submit" sx={signInButtonStyle}>
-              התחבר
-            </Button>
-          </Box>
-          <FirstSignInDialog email={formData.email} open={isFirstTime} onClose={() => setIsFirstTime(false)} />
-          <Box sx={{ marginTop: "10px", textAlign: "center" }}>
-            <Link href="#" variant="body2" sx={forgotPasswordStyle}>
-              שכחת את הסיסמה?
-            </Link>
-          </Box>
+                    <TextField
+                        label="סיסמה"
+                        variant="outlined"
+                        type="password"
+                        fullWidth
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        error={!!errors.password}
+                        helperText={errors.password}
+                        sx={signInTextFieldStyle}
+                    />
+                    <Box sx={{ display: "flex", justifyContent: "center" }}>
+                        <Button variant="contained" type="submit" sx={signInButtonStyle}>
+                            התחבר
+                        </Button>
+                    </Box>
+                    <FirstSignInDialog email={formData.email} open={isFirstTime} onClose={() => setIsFirstTime(false)} />
+                    <Box sx={{ marginTop: "10px", textAlign: "center" }}>
+                        <Link href="#" variant="body2" sx={forgotPasswordStyle}>
+                            שכחת את הסיסמה?
+                        </Link>
+                    </Box>
+                </Box>
+            </CacheProvider>
         </Box>
-      </CacheProvider>
-    </Box>
-  );
+    );
 };
 
 export default SignIn;
