333a8cc1aa68b18b077e2817e1df91cb763215de
edengal283 <eden283gal@gmail.com>
2025-03-06 06:47:49 +0200

    fix private route

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,14 +1,15 @@
 import { Navigate } from 'react-router-dom';
 import { useUser } from '../../contexts/userContext';
 interface PrivateRouteProps {
-    element: React.ReactNode; 
+    element: React.ReactNode;
 }
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ element }) => {
 
-    const { user } = useUser(); 
+    const { user } = useUser();
+    const cachedUser = localStorage.getItem("user")
 
-    if (!user) {
+    if (!user && !cachedUser) {
         return <Navigate to="/login" />;
     }
 
