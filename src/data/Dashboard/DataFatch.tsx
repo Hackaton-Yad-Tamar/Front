50be40f7451068b3b12fab402a379559219d978d
Shalevzzz <shalevaviv13@gmail.com>
2025-03-06 02:37:43 +0200

    table

--- src/data/Dashboard/DataFatch.tsx
+++ src/data/Dashboard/DataFatch.tsx
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 const DASHBOARD_ENDPOINT = 'http://localhost:8000/admin/dashboard/';
 
-// export const tableData = [
-//     { id: 1, name: "Item 1", price: 100, city: "New York", category: "Electronics", value: 30 },
-//     { id: 2, name: "Item 2", price: 150, city: "Los Angeles", category: "Clothing", value: 40 },
-//     { id: 3, name: "Item 3", price: 200, city: "New York", category: "Books", value: 60 },
-//     { id: 4, name: "Item 4", price: 120, city: "Los Angeles", category: "Electronics", value: 90 },
-//     { id: 5, name: "Item 5", price: 80, city: "Chicago", category: "Books", value: 50 },
-//     { id: 6, name: "Item 6", price: 90, city: "Chicago", category: "Clothing", value: 20 },
-//   ];
+export const tableData = [
+    { id: 1, name: "Item 1", price: 100, city: "New York", category: "Electronics", value: 30 },
+    { id: 2, name: "Item 2", price: 150, city: "Los Angeles", category: "Clothing", value: 40 },
+    { id: 3, name: "Item 3", price: 200, city: "New York", category: "Books", value: 60 },
+    { id: 4, name: "Item 4", price: 120, city: "Los Angeles", category: "Electronics", value: 90 },
+    { id: 5, name: "Item 5", price: 80, city: "Chicago", category: "Books", value: 50 },
+    { id: 6, name: "Item 6", price: 90, city: "Chicago", category: "Clothing", value: 20 },
+  ];
 
 export type PieParameters = 'city' | 'request_type' | 'status';
 
@@ -23,7 +23,7 @@ export interface PieData {
 
 export const fetchPieData = async (function_name: string, params: PieData) => {
     const response = await axios.get(DASHBOARD_ENDPOINT + function_name, { params });
-    
+    console.log("response",response)
     if (response.status !== 200) {
         throw new Error('Failed to fetch data');
     }
--- src/views/DashboardView/DashboardView.tsx
+++ src/views/DashboardView/DashboardView.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
+
+//componants
 import { PieChartComponent } from "../../components/Dashboard/PieChart";
 import { BasicDatePicker } from "../../components/Dashboard/DateContainer";
+import { CustomBarChart } from "../../components/Dashboard/BarChart";
+import { DataTable } from "../../components/Dashboard/DataTable";
 
-
-import { fetchPieData, PieData, PieParameters } from "../../data/Dashboard/DataFatch"; // Assuming data is in TestData
+//data
+import { fetchPieData, PieData, PieParameters ,tableData} from "../../data/Dashboard/DataFatch"; // Assuming data is in TestData
 
 export const Dashboard = () => {
   const [filters, setFilters] = useState<PieData>({
@@ -39,6 +43,8 @@ export const Dashboard = () => {
   console.log("filters:", filters); // Add log to check data
   console.log("Filtered Status Data:", statusData); // Add log to check data
   console.log("Filtered City Data:", cityData); // Add log to check data
+  console.log("Filtered Type Data:", typeData); // Add log to check data
+  console.log("tableData:", tableData); // Add log to check data
 
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
@@ -74,8 +80,12 @@ export const Dashboard = () => {
             onPieClick={(request_type) => handleFilter("request_type", request_type)}
           />
         </div>
+        <CustomBarChart
+          data={typeData}
+        />
       </div>
-      {/* <DataTable data={filteredTableData} /> */}
+      <DataTable 
+        data={tableData}      />
     </div>
   );
 };
